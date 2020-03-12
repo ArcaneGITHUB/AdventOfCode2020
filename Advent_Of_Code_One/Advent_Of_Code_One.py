38c4d2856f60f6f03c401eb7b0f8e34ebd33f426
David <44246360+DavidGTHB@users.noreply.github.com>
2020-03-12 17:02:06 +0000

    Changed code to answer part 2

--- Advent_Of_Code_One/Advent_Of_Code_One.py
+++ Advent_Of_Code_One/Advent_Of_Code_One.py
@@ -1,6 +1,19 @@
+
+
+def fuel_requirement(mass):
+    extra_fuel = (mass // 3) - 2
+    if extra_fuel > 0:
+        extra_fuel += fuel_requirement(extra_fuel)
+    else:
+        return 0
+    return extra_fuel
+
+
 input_file = open("input.txt", "r")
 module_list = input_file.read().splitlines()
 fuel = 0
+
 for module in module_list:
-    fuel += (int(module)//3)-2
+    fuel += fuel_requirement(int(module))
+
 print(fuel)
