c96552d8b2d8fa549a8af7853c4ce7974ae03a74
David <44246360+DavidGTHB@users.noreply.github.com>
2020-03-12 14:41:05 +0000

    IMPROVED CODE 300%

--- Advent_Of_Code_One/Advent_Of_Code_One.py
+++ Advent_Of_Code_One/Advent_Of_Code_One.py
@@ -1,7 +1,6 @@
 input_file = open("input.txt", "r")
 module_list = input_file.read().splitlines()
-fuel_list = []
+fuel = 0
 for module in module_list:
-    module = int(module)
-    fuel_list.append(int(module/3)-2)
-print(sum(fuel_list))
+    fuel += (int(module)//3)-2
+print(fuel)
