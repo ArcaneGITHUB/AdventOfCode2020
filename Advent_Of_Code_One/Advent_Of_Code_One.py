aa523fa957bbd552eca3f532f39268f16626a862
David <44246360+DavidGTHB@users.noreply.github.com>
2020-03-16 17:13:51 +0000

    Made slight improvements to readability and fixed memory leak.

--- Advent_Of_Code_One/Advent_Of_Code_One.py
+++ Advent_Of_Code_One/Advent_Of_Code_One.py
@@ -3,17 +3,16 @@
 def fuel_requirement(mass):
     extra_fuel = (mass // 3) - 2
     if extra_fuel > 0:
-        extra_fuel += fuel_requirement(extra_fuel)
+        return extra_fuel + fuel_requirement(extra_fuel)
     else:
         return 0
-    return extra_fuel
 
 
-input_file = open("input.txt", "r")
-module_list = input_file.read().splitlines()
-fuel = 0
+with open("input.txt", "r") as input_file:
+    module_list = input_file.read().splitlines()
+total_fuel = 0
 
 for module in module_list:
-    fuel += fuel_requirement(int(module))
+    total_fuel += fuel_requirement(int(module))
 
-print(fuel)
+print(total_fuel)
