4233680989f4d567c9f807a507d108d18cff8a2d
David <44246360+DavidGTHB@users.noreply.github.com>
2020-03-20 23:39:21 +0000

    finished part 2. With the help of a friend

--- Advent_Of_Code_2/Advent_Of_Code_2.py
+++ Advent_Of_Code_2/Advent_Of_Code_2.py
@@ -1,22 +1,22 @@
 
 
-def run_as_intcode(intlist, instruction):
+def run_intcode(intlist, instruction):
     if intlist[instruction] == 1:
         intlist[intlist[instruction+3]] = intlist[intlist[instruction+1]] + intlist[intlist[instruction+2]]
+        run_intcode(intlist, instruction + 4)
     elif intlist[instruction] == 2:
         intlist[intlist[instruction + 3]] = intlist[intlist[instruction + 1]] * intlist[intlist[instruction + 2]]
+        run_intcode(intlist, instruction + 4)
     elif intlist[instruction] == 99:
+        print(intlist[0])
         print("99 found. Program ending.")
         return 0
     else:
         print("Unknown code found. Program ending.")
         return 1
 
-    instruction += 4
-    run_as_intcode(intlist, instruction)
-
 
 with open("input.txt", "r") as input_file:
     intcode_list = input_file.read().split(",")
-    print(intcode_list)
-run_as_intcode(intcode_list, 0)
+    intcode_list = list(map(int, intcode_list))
+run_intcode(intcode_list, 0)
---
+++ Advent_Of_Code_2/Advent_Of_Code_2p2.py
@@ -0,0 +1,24 @@
+
+
+def run_intcode(intlist, instruction):
+    if intlist[instruction] == 1:
+        intlist[intlist[instruction+3]] = intlist[intlist[instruction+1]] + intlist[intlist[instruction+2]]
+        return run_intcode(intlist, instruction + 4)
+    elif intlist[instruction] == 2:
+        intlist[intlist[instruction + 3]] = intlist[intlist[instruction + 1]] * intlist[intlist[instruction + 2]]
+        return run_intcode(intlist, instruction + 4)
+    elif intlist[instruction] == 99:
+        return intlist[0]
+    else:
+        return 1
+
+
+for noun in range(100):
+    for verb in range(100):
+        with open("input.txt", "r") as input_file:
+            intcode_list = list(map(int, input_file.read().split(",")))
+        intcode_list[1] = noun
+        intcode_list[2] = verb
+        if run_intcode(intcode_list, 0) == 19690720:
+            print(noun)
+            print(verb)
