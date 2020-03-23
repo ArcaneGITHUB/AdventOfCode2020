4897e59460f957e3f4903004af9f1c5f517d5a11
David <44246360+DavidGTHB@users.noreply.github.com>
2020-03-23 17:48:08 +0000

    Finished day 3

--- Advent_Of_Code_3/Advent_Of_Code_3.py
+++ Advent_Of_Code_3/Advent_Of_Code_3.py
@@ -1,13 +1,42 @@
-import numpy as np
 
 with open("input.txt", "r") as input_file:
     wires = input_file.read().splitlines()
     wire_one = wires[0].split(",")
     wire_two = wires[1].split(",")
-    print(wire_one)
-    print(wire_two)
 
-wire_map = np.full((10000, 10000), 0)
-print(type(wire_map))
 
-print(wire_map.size)
+def plot(wire):
+    coordinate_list = []
+    current_location = (0, 0)
+    for section in wire:
+        direction = section[0]
+        section = int(section[1:])
+        if direction == "U":
+            for y in range(1, section+1):
+                coordinate_list.append((current_location[0], current_location[1]+y))
+        elif direction == "D":
+            for y in range(1, section+1):
+                coordinate_list.append((current_location[0], current_location[1]-y))
+        elif direction == "R":
+            for x in range(1, section+1):
+                coordinate_list.append((current_location[0]+x, current_location[1]))
+        elif direction == "L":
+            for x in range(1, section+1):
+                coordinate_list.append((current_location[0]-x, current_location[1]))
+        current_location = coordinate_list[-1]
+    return coordinate_list
+
+
+def manhattan_distance(point_one, point_two):
+    return abs(point_two[0] - point_one[0]) + abs(point_two[1] - point_one[1])
+
+
+wire_one = plot(wire_one)
+wire_two = plot(wire_two)
+
+lowest_distance = 999999999999999
+for point in set(wire_one).intersection(wire_two):
+    distance = manhattan_distance((0, 0), point)
+    if distance < lowest_distance:
+        lowest_distance = distance
+print(lowest_distance)
