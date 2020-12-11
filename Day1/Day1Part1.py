caac41440800d099f50d1f5333db4eb331aa1c62
David <44246360+DavidGTHB@users.noreply.github.com>
2020-12-11 17:26:37 +0000

    Corrected Day1 and shortened answer

--- Day1/Day1Part1.py
+++ Day1/Day1Part1.py
@@ -2,13 +2,10 @@ sum_goal = 2020
 
 with open("input.txt", "r") as input_file:
     input_list = [int(num) for num in input_file.read().splitlines()]     # Convert strings to ints
-    input_list.sort()
 
     success = False
-    while not success:
+    while not success and input_list:
         num = input_list.pop(0)
-        for second_num in input_list:
-            sum = num + second_num
-            if (2020 - sum) in input_list:
-                print(num * second_num * (2020-sum))
-                success = True
+        if (2020 - num) in input_list:
+            print(num * (2020-num))
+            success = True
