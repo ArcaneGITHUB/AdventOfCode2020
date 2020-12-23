2267b02ae0e0efcf7313cf52055448e821ac3cde
David <44246360+DavidGTHB@users.noreply.github.com>
2020-12-23 01:17:11 +0000

    Optimized file opening process

--- day10/day10part1.py
+++ day10/day10part1.py
@@ -5,15 +5,15 @@
 
 with open("input.txt", "r") as adapter_outputs:
     adapter_outputs = adapter_outputs.read().splitlines()
-    adapter_outputs = sorted([int(joltage) for joltage in adapter_outputs])
-    adapter_outputs.insert(0, 0) # Account for socket voltage
-    jolt_diffs = {1: 0, 3: 1}   # Always an additional gap of 3 between final adapter and device
-    for index, adapter in enumerate(adapter_outputs[:-1]):
-        next_adapter = adapter_outputs[index+1]
-        next_difference = next_adapter - adapter
-        if next_difference == 1:
-            jolt_diffs[1] += 1
-        elif next_difference == 3:
-            jolt_diffs[3] += 1
+adapter_outputs = sorted([int(joltage) for joltage in adapter_outputs])
+adapter_outputs.insert(0, 0) # Account for socket voltage
+jolt_diffs = {1: 0, 3: 1}   # Always an additional gap of 3 between final adapter and device
+for index, adapter in enumerate(adapter_outputs[:-1]):
+    next_adapter = adapter_outputs[index+1]
+    next_difference = next_adapter - adapter
+    if next_difference == 1:
+        jolt_diffs[1] += 1
+    elif next_difference == 3:
+        jolt_diffs[3] += 1
 
-    print(jolt_diffs[1] * jolt_diffs[3])
+print(jolt_diffs[1] * jolt_diffs[3])
