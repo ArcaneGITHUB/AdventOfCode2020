b918820541a1c79c31fe94764795c85819cbc9f4
David <44246360+DavidGTHB@users.noreply.github.com>
2021-12-02 18:59:47 +0000

    Completed Day 2

---
+++ 2021/day2/day2part1.py
@@ -0,0 +1,29 @@
+# https://adventofcode.com/2021/day/2
+
+with open("input", "r") as input_file:
+    instruction_list = []
+    for instruction in input_file:
+        instruction = instruction.split()
+        instruction[1] = int(instruction[1])
+        instruction_list.append(instruction)
+
+# Test input
+# instruction_list = [("forward", 5),
+#                     ("down", 5),
+#                     ("forward", 8),
+#                     ("up", 3),
+#                     ("down", 8),
+#                     ("forward", 2)]
+
+horizontal = 0
+depth = 0
+
+for instruction in instruction_list:
+    if instruction[0] == "forward":
+        horizontal += instruction[1]
+    elif instruction[0] == "down":
+        depth += instruction[1]
+    elif instruction[0] == "up":
+        depth -= instruction[1]
+
+print(horizontal * depth)
